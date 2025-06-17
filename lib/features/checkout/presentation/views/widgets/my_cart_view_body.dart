dfc6faaee37c27db3340cb7090af93b830266670
Mohamed Ali <a74498722@gmail.com>
2025-06-16 19:36:27 -0700

    feat: restructure MyCartViewBody layout and add OrderInfoItem widget

--- lib/features/checkout/presentation/views/widgets/my_cart_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/my_cart_view_body.dart
@@ -1,16 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:paymentgateways_app/core/utils/styles.dart';
 
 class MyCartViewBody extends StatelessWidget {
   const MyCartViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 18,
+          ),
+          Image.asset('assets/images/basket_image.png'),
+          const SizedBox(
+            height: 25,
+          ),
+          const OrderInfoItem(
+            title: 'Order Subtotal',
+            value: '\$42.97',
+          ),
+          const SizedBox(
+            height: 3,
+          ),
+          const OrderInfoItem(
+            title: 'Discount',
+            value: '\$0',
+          ),
+          const SizedBox(
+            height: 3,
+          ),
+          const OrderInfoItem(
+            title: 'Shipping',
+            value: '\$8',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OrderInfoItem extends StatelessWidget {
+  const OrderInfoItem({super.key, required this.title, required this.value});
+
+  final String title, value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
       children: [
-        const SizedBox(
-          height: 18,
+        Text(
+          title,
+          style: Styles.style18,
         ),
-        Image.asset('assets/images/basket_image.png'),
+        const Spacer(),
+        Text(
+          value,
+          style: Styles.style18,
+        ),
       ],
     );
   }
