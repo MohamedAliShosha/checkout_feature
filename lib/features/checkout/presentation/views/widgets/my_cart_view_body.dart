a1d59d67ee973c8051e0db60361fb4b84064fc14
Mohamed Ali <a74498722@gmail.com>
2025-06-16 19:40:04 -0700

    feat: move OrderInfoItem widget to its own file for better organization

--- lib/features/checkout/presentation/views/widgets/my_cart_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/my_cart_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:paymentgateways_app/core/utils/styles.dart';
+import 'package:paymentgateways_app/features/checkout/presentation/views/widgets/order_info_item.dart';
 
 class MyCartViewBody extends StatelessWidget {
   const MyCartViewBody({super.key});
@@ -37,29 +37,6 @@ class MyCartViewBody extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class OrderInfoItem extends StatelessWidget {
-  const OrderInfoItem({super.key, required this.title, required this.value});
-
-  final String title, value;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Text(
-          title,
-          style: Styles.style18,
-        ),
-        const Spacer(),
-        Text(
-          value,
-          style: Styles.style18,
-        ),
-      ],
     );
   }
 }
