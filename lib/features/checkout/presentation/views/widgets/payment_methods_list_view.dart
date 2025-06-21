705ff1aa35b71f797fac280b354b8f0fcc046bdf
Mohamed Ali <a74498722@gmail.com>
2025-06-21 08:26:52 -0700

    feat: convert PaymentMethodsListView to StatefulWidget for dynamic active payment method handling

--- lib/features/checkout/presentation/views/widgets/payment_methods_list_view.dart
+++ lib/features/checkout/presentation/views/widgets/payment_methods_list_view.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:paymentgateways_app/features/checkout/presentation/views/widgets/payment_method_item.dart';
 
-class PaymentMethodsListView extends StatelessWidget {
+class PaymentMethodsListView extends StatefulWidget {
   const PaymentMethodsListView({super.key});
 
+  @override
+  State<PaymentMethodsListView> createState() => _PaymentMethodsListViewState();
+}
+
+class _PaymentMethodsListViewState extends State<PaymentMethodsListView> {
   final List<String> paymentMethodsItems = const [
     'assets/images/card.svg',
     'assets/images/paypal.svg',
   ];
+
+  int activeIndex =
+      0; // Index of the active payment method its by default the first one
 
   @override
   Widget build(BuildContext context) {
@@ -21,9 +29,16 @@ class PaymentMethodsListView extends StatelessWidget {
             padding: const EdgeInsets.symmetric(
               horizontal: 8,
             ),
-            child: PaymentMethodItem(
-              image: paymentMethodsItems[index],
-              isActive: false,
+            child: GestureDetector(
+              onTap: () {
+                activeIndex = index;
+                setState(() {});
+              },
+              child: PaymentMethodItem(
+                image: paymentMethodsItems[index],
+                isActive: activeIndex ==
+                    index, // Check if the current index is the active index
+              ),
             ),
           );
         },
