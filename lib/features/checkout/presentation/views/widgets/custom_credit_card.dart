0891b46eb4f07c70369d41a90eaca401bad420ab
Mohamed Ali <a74498722@gmail.com>
2025-06-21 09:27:05 -0700

    feat: implement CustomCreditCard widget for enhanced credit card input handling

---
+++ lib/features/checkout/presentation/views/widgets/custom_credit_card.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+
+class CustomCreditCard extends StatefulWidget {
+  const CustomCreditCard({super.key});
+
+  @override
+  State<CustomCreditCard> createState() => _CustomCreditCardState();
+}
+
+class _CustomCreditCardState extends State<CustomCreditCard> {
+  String cardNumber =
+      ''; // these variables must have initial values, In the beginning they are empty
+  String expiryDate = '';
+  String cardHolderName = '';
+  String cvvCode = '';
+  bool showBackView = false;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CreditCardWidget(
+          isHolderNameVisible: true, // show the name of the card holder
+          cardNumber: cardNumber,
+          expiryDate: expiryDate,
+          cardHolderName: cardHolderName,
+          cvvCode: cvvCode,
+          showBackView: showBackView,
+          onCreditCardWidgetChange: (value) {},
+        ),
+        CreditCardForm(
+          cardNumber: cardNumber,
+          expiryDate: expiryDate,
+          cardHolderName: cardHolderName,
+          cvvCode: cvvCode,
+          onCreditCardModelChange:
+              (CreditCardModel) // CreditCardModel includes all data I concern about then through the model I can access data comes from the form then assigning them to the variables I have created above then making rebuild to display the new data
+              {
+            cardHolderName = CreditCardModel.cardHolderName;
+            cardNumber = CreditCardModel.cardNumber;
+            expiryDate = CreditCardModel.expiryDate;
+            cvvCode = CreditCardModel.cvvCode;
+            showBackView = CreditCardModel
+                .isCvvFocused; // show back side when pressing on the cvv field to enter it.
+            setState(() {});
+          },
+          formKey: formKey,
+        )
+      ],
+    );
+  }
+}
