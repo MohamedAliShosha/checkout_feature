7c2b43c201a724c0009d851408c09d2002d5fb17
Mohamed Ali <a74498722@gmail.com>
2025-06-21 09:25:00 -0700

    feat: integrate CreditCard widget and form into PaymentDetailsViewBody for enhanced payment processing

--- lib/features/checkout/presentation/views/widgets/payment_details_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/payment_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
 import 'package:paymentgateways_app/features/checkout/presentation/views/widgets/payment_methods_list_view.dart';
 
 class PaymentDetailsViewBody extends StatelessWidget {
@@ -6,9 +7,65 @@ class PaymentDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return const SingleChildScrollView(
+      physics: BouncingScrollPhysics(),
+      child: Column(
+        children: [
+          PaymentMethodsListView(),
+          CustomCreditCard(),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomCreditCard extends StatefulWidget {
+  const CustomCreditCard({super.key});
+
+  @override
+  State<CustomCreditCard> createState() => _CustomCreditCardState();
+}
+
+class _CustomCreditCardState extends State<CustomCreditCard> {
+  String cardNumber =
+      ''; // these variables must have initial values, In the beginning they are empty
+  String expiryDate = '';
+  String cardHolderName = '';
+  String cvvCode = '';
+  bool showBackView = false;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
       children: [
-        PaymentMethodsListView(),
+        CreditCardWidget(
+          isHolderNameVisible: true, // show the name of the card holder
+          cardNumber: cardNumber,
+          expiryDate: expiryDate,
+          cardHolderName: cardHolderName,
+          cvvCode: cvvCode,
+          showBackView: showBackView,
+          onCreditCardWidgetChange: (value) {},
+        ),
+        CreditCardForm(
+          cardNumber: cardNumber,
+          expiryDate: expiryDate,
+          cardHolderName: cardHolderName,
+          cvvCode: cvvCode,
+          onCreditCardModelChange:
+              (CreditCardModel) // CreditCardModel includes all data I concern about then through the model I can access data comes from the form then assigning them to the variables I have created above then making rebuild to display the new data
+              {
+            cardHolderName = CreditCardModel.cardHolderName;
+            cardNumber = CreditCardModel.cardNumber;
+            expiryDate = CreditCardModel.expiryDate;
+            cvvCode = CreditCardModel.cvvCode;
+            showBackView = CreditCardModel
+                .isCvvFocused; // show back side when pressing on the cvv field to enter it.
+            setState(() {});
+          },
+          formKey: formKey,
+        )
       ],
     );
   }
