0d0e673139dd90ea8f2d1ea3d173d3d53936afb3
Mohamed Ali <a74498722@gmail.com>
2025-06-26 15:07:26 -0700

    feat: add ThankYouCard and PaymentItemInfo widgets for enhanced checkout experience

--- lib/features/checkout/presentation/views/widgets/thank_you_card.dart
+++ lib/features/checkout/presentation/views/widgets/thank_you_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:paymentgateways_app/core/utils/styles.dart';
+import 'package:paymentgateways_app/features/checkout/presentation/views/widgets/payment_item_info.dart';
 
 class ThankYouCard extends StatelessWidget {
   const ThankYouCard({
@@ -8,10 +10,54 @@ class ThankYouCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity, // To take the available space
       decoration: ShapeDecoration(
         color: const Color(0xffD9D9D9),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(
+          top: 50 +
+              16, // 50 is the radius of the circle , and 16 is tha actual padding between the circle and the text
+          right: 22,
+          left: 22,
+        ),
+        child: Column(
+          children: [
+            const Text(
+              'Thank You',
+              textAlign: TextAlign.center,
+              style: Styles.style25,
+            ),
+            Text(
+              'Your transaction was successful',
+              textAlign: TextAlign.center,
+              style: Styles.style20,
+            ),
+            const SizedBox(
+              height: 42,
+            ),
+            const PaymentItemInfo(
+              title: 'Date',
+              value: '01/24/2023',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const PaymentItemInfo(
+              title: 'Time',
+              value: '10:15 AM',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const PaymentItemInfo(
+              title: 'To',
+              value: 'Sam Louis',
+            ),
+          ],
         ),
       ),
     );
