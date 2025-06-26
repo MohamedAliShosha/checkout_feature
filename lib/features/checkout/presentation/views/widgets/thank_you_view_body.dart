981b0b3952570437e914830ecb75d9b6cc4108c9
Mohamed Ali <a74498722@gmail.com>
2025-06-26 14:47:53 -0700

    feat: refactor ThankYouViewBody layout with custom widgets for improved readability and maintainability

--- lib/features/checkout/presentation/views/widgets/thank_you_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/thank_you_view_body.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:paymentgateways_app/features/checkout/presentation/views/widgets/custom_check_icon.dart';
+import 'package:paymentgateways_app/features/checkout/presentation/views/widgets/custom_dashed_line.dart';
+import 'package:paymentgateways_app/features/checkout/presentation/views/widgets/thank_you_card.dart';
 
 class ThankYouViewBody extends StatelessWidget {
   const ThankYouViewBody({super.key});
@@ -11,34 +14,14 @@ class ThankYouViewBody extends StatelessWidget {
         child: Stack(
           clipBehavior: Clip.none,
           children: [
-            Container(
-              decoration: ShapeDecoration(
-                color: const Color(0xffD9D9D9),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-            ),
+            const ThankYouCard(),
             Positioned(
               bottom: MediaQuery.sizeOf(context).height * .2 +
                   20, // bottom moves the circle up , 20 is the radius of the circle
               left: 20 + 8,
               right: 20 +
                   8, // 20 is the radius of the circle and 16 is the padding of the container
-              child: Row(
-                children: List.generate(
-                  30,
-                  (index) => Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 2),
-                      child: Container(
-                        height: 2,
-                        color: const Color(0xffBBBBBB),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+              child: const CustomDashedLine(),
             ),
             Positioned(
               left:
@@ -64,19 +47,7 @@ class ThankYouViewBody extends StatelessWidget {
               top: -50,
               left: 0, // Taking No space from the left
               right: 0, // Taking No space from the right
-              child: CircleAvatar(
-                radius: 50,
-                backgroundColor: Color(0xffD9D9D9),
-                child: CircleAvatar(
-                  radius: 40,
-                  backgroundColor: Color(0xff34AB53),
-                  child: Icon(
-                    Icons.check,
-                    size: 50,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
+              child: CustomCheckIcon(),
             ),
           ],
         ),
