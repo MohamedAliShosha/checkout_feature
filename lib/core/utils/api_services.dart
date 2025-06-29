9f9a1b03d58c7753302e74354f43690ee841d02a
Mohamed Ali <a74498722@gmail.com>
2025-06-29 09:19:34 -0700

    feat: implement Api service with CRUD methods and add StripeService class

---
+++ lib/core/utils/api_services.dart
@@ -0,0 +1,103 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+
+class Api {
+  final Dio _dio = Dio();
+
+  // @required is an annotation that was used to indicate that a parameter must be provided when calling the function, even if it is nullable
+  Future<dynamic> get({required String url, @required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers['Authorization'] = 'Bearer $token';
+    }
+    try {
+      final response = await _dio.get(
+        url,
+        options: Options(headers: headers),
+      );
+      return response.data;
+    } on DioException catch (e) {
+      throw Exception(
+          'GET request failed: ${e.response?.statusCode} ${e.message}');
+    }
+  }
+
+  Future<dynamic> post({
+    required dynamic body,
+    required String url,
+    String? contentType,
+    required String? token, // token is required but tha value may be null
+  }) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers['Authorization'] =
+          'Bearer $token'; // adding the token to the header
+      headers['Content-Type'] = contentType ??
+          'application/json'; // adding the content type to the header
+    }
+    try {
+      final response = await _dio.post(
+        url,
+        data: body, // passing the body that will be sent
+        options: Options(
+            headers:
+                headers), // options holds the headers that contains "token and content type"
+      );
+      return response
+          .data; // response.data contains only the actual data returned from the server (usually the JSON body or result you care about).
+    } on DioException catch (e) {
+      throw Exception(
+          'POST request failed: ${e.response?.statusCode} ${e.message}');
+    }
+  }
+
+  Future<dynamic> put({
+    required String url,
+    @required dynamic body,
+    @required String? token,
+  }) async {
+    Map<String, String> headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    };
+    if (token != null) {
+      headers['Authorization'] = 'Bearer $token';
+    }
+    log('url = $url body = $body token = $token ');
+    try {
+      final response = await _dio.put(
+        url,
+        data: body,
+        options: Options(headers: headers),
+      );
+      if (kDebugMode) {
+        print(response.data);
+      }
+      return response.data;
+    } on DioException catch (e) {
+      throw Exception(
+          'PUT request failed: ${e.response?.statusCode} ${e.message}');
+    }
+  }
+
+  Future<dynamic> delete({
+    required String url,
+    @required String? token,
+  }) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers['Authorization'] = 'Bearer $token';
+    }
+    try {
+      final response = await _dio.delete(
+        url,
+        options: Options(headers: headers),
+      );
+      return response.data;
+    } on DioException catch (e) {
+      throw Exception(
+          'DELETE request failed: ${e.response?.statusCode} ${e.message}');
+    }
+  }
+}
---
+++ lib/core/utils/stripe_service.dart
@@ -0,0 +1,9 @@
+import 'package:paymentgateways_app/features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:paymentgateways_app/features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
+
+class StripeService {
+  // PaymentIntentModel createPaymentIntent(PaymentIntentInputModel paymentIntentInputModel)
+  // {
+
+  // }
+}
