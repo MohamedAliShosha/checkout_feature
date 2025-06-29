bbf9bede078d3d61e7f2ae46fe89df34fd6d3243
Mohamed Ali <a74498722@gmail.com>
2025-06-29 10:26:18 -0700

    feat: add await to initPaymentSheet method for proper asynchronous handling

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -23,11 +23,15 @@ class StripeService {
   }
 
   Future initPaymentSheet({required String paymentIntentClientSecret}) async {
-    Stripe.instance.initPaymentSheet(
+    await Stripe.instance.initPaymentSheet(
       paymentSheetParameters: SetupPaymentSheetParameters(
         paymentIntentClientSecret: paymentIntentClientSecret,
         merchantDisplayName: 'Mohamed',
       ),
     );
+  }
+
+  Future displayPaymentSheet() async {
+    await Stripe.instance.presentPaymentSheet();
   }
 }
