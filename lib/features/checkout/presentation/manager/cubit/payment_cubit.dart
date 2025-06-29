eafb3b80e20d85699512de3a46b9dca4a5bc896d
Mohamed Ali <a74498722@gmail.com>
2025-06-29 11:25:47 -0700

    feat: add PaymentCubit and PaymentState for payment processing management

---
+++ lib/features/checkout/presentation/manager/cubit/payment_cubit.dart
@@ -0,0 +1,42 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:paymentgateways_app/features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:paymentgateways_app/features/checkout/data/repos/checkout_repo.dart';
+
+part 'payment_state.dart';
+
+class PaymentCubit extends Cubit<PaymentState> {
+  PaymentCubit(this.checkoutRepo) : super(PaymentInitial());
+
+  final CheckoutRepo checkoutRepo;
+
+  Future makePayment(
+      {required PaymentIntentParametersModel
+          paymentIntentParametersModel}) async {
+    emit(PaymentLoading());
+    var data = await checkoutRepo.makePayment(
+        paymentIntentParametersModel: paymentIntentParametersModel);
+
+    data.fold(
+      (failure) {
+        emit(
+          PaymentFailure(failure.errorMessage),
+        );
+      },
+      (success) {
+        emit(
+          PaymentSuccess(),
+        );
+      },
+    );
+  }
+
+  // As it only one cubit we just override the onChange method instead of creating entire bloc observer
+  @override
+  void onChange(Change<PaymentState> change) {
+    log(change.toString()); // used to monitor the cubit changes then print them
+    super.onChange(change);
+  }
+}
