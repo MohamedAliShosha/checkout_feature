8f0667908b86ded2743d8441b42c533fac2ed98c
Mohamed Ali <a74498722@gmail.com>
2025-06-29 10:51:19 -0700

    feat: implement CheckoutRepo for payment processing with Stripe integration

---
+++ lib/features/checkout/data/repos/checkout_repo_implement.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:paymentgateways_app/core/errors/failures.dart';
+import 'package:paymentgateways_app/core/utils/stripe_service.dart';
+import 'package:paymentgateways_app/features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:paymentgateways_app/features/checkout/data/repos/checkout_repo.dart';
+
+class CheckoutRepoImplement implements CheckoutRepo {
+  final StripeService stripeService = StripeService();
+
+  @override
+  Future<Either<Failures, void>> makePayment(
+      {required PaymentIntentParametersModel
+          paymentIntentParametersModel}) async {
+    try {
+      await stripeService.makePayment(
+          paymentIntentParametersModel: paymentIntentParametersModel);
+      return const Right(null); // as the payment is made successfully
+    } catch (e) {
+      return Left(
+        ServerFailure(
+          e.toString(),
+        ),
+      ); // as the payment failed
+    }
+  }
+}
