0e24942524827a89bbab02c970cd3627524ffe12
Mohamed Ali <a74498722@gmail.com>
2025-07-01 10:28:49 -0700

    feat: refactor payment processing logic in CustomBlocConsumerButton; extract transaction data handling and improve PayPal payment execution

--- lib/features/checkout/presentation/views/widgets/custom_bloc_consumer_button.dart
+++ lib/features/checkout/presentation/views/widgets/custom_bloc_consumer_button.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
+import 'package:paymentgateways_app/core/utils/api_keys.dart';
 import 'package:paymentgateways_app/core/widgets/custom_button.dart';
 import 'package:paymentgateways_app/features/checkout/data/models/amount_model/amount_model.dart';
 import 'package:paymentgateways_app/features/checkout/data/models/amount_model/details.dart';
@@ -52,76 +53,92 @@ class CustomBlocConsumerButton extends StatelessWidget {
             //   paymentIntentParametersModel: parametersModel,
             // );
 
-            /*
-            - first I created an amountModel then I gave values to its attributes
-            */
-            var amount = AmountModel(
-              total: '100',
-              currency: 'USD',
-              details: DetailsModel(
-                shipping: '0',
-                shippingDiscount: 0,
-                subtotal: '100',
-              ),
-            );
-            /*
-              - Second I Created a list of orders each one of them is an OrderItemModel
-            */
-            List<OrderItemModel> orders = [
-              OrderItemModel(
-                currency: 'USD',
-                name: 'Apple',
-                price: '4',
-                quantity: 10,
-              ),
-              OrderItemModel(
-                currency: 'USD',
-                name: 'Apple',
-                price: '5',
-                quantity: 12,
-              ),
-            ];
-            /*
-              - Third I created an ItemList of Type ItemListModel and this model holds all orders
-             */
-            var itemList = ItemListModel(orders: orders);
+            var transactionsData = getTransactionData();
 
-            Navigator.of(context).push(MaterialPageRoute(
-              // PaypalCheckoutView is an InApp web view
-              builder: (BuildContext context) => PaypalCheckoutView(
-                sandboxMode: true, // true => Test Mode , false => Live Mode
-                clientId: "YOUR CLIENT ID",
-                secretKey: "YOUR SECRET KEY",
-                // Transaction is a list that contains a map
-                // These map has 3 keys "amount, currency and description"
-                // Each key has a value
-                transactions: [
-                  {
-                    "amount": amount.toJson(),
-                    "description": "The payment transaction description.",
-                    "item_list": itemList.toJson(),
-                  }
-                ],
-                note: "Contact us for any questions on your order.",
-                onSuccess: (Map params) async {
-                  log("onSuccess: $params");
-                  Navigator.pop(context);
-                },
-                onError: (error) {
-                  log("onError: $error");
-                  Navigator.pop(context);
-                },
-                onCancel: () {
-                  print('cancelled:');
-                  Navigator.pop(context);
-                },
-              ),
-            ));
+            executePayPalPayment(context, transactionsData);
           },
           isLoading: state is PaymentLoading ? true : false,
           title: 'Continue',
         );
       },
     );
+  }
+
+  void executePayPalPayment(BuildContext context,
+      ({AmountModel amount, ItemListModel itemList}) transactionsData) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        // PaypalCheckoutView is an InApp web view
+        builder: (BuildContext context) => PaypalCheckoutView(
+          sandboxMode: true, // true => Test Mode , false => Live Mode
+          clientId: ApiKeys.payPalClientId,
+          secretKey: ApiKeys.payPalSecretKey,
+          // Transaction is a list that contains a map
+          // These map has 3 keys "amount, currency and description"
+          // Each key has a value
+          transactions: [
+            {
+              "amount": transactionsData.amount.toJson(),
+              "description": "The payment transaction description.",
+              "item_list": transactionsData.itemList.toJson(),
+            }
+          ],
+          note: "Contact us for any questions on your order.",
+          onSuccess: (Map params) async {
+            log("onSuccess: $params");
+            Navigator.pop(context);
+          },
+          onError: (error) {
+            log("onError: $error");
+            Navigator.pop(context);
+          },
+          onCancel: () {
+            print('cancelled:');
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+
+  ({AmountModel amount, ItemListModel itemList}) getTransactionData() {
+    /*
+            - first I created an amountModel then I gave values to its attributes
+            */
+    var amount = AmountModel(
+      total: '100',
+      currency: 'USD',
+      details: DetailsModel(
+        shipping: '0',
+        shippingDiscount: 0,
+        subtotal: '100',
+      ),
+    );
+    /*
+              - Second I Created a list of orders each one of them is an OrderItemModel
+            */
+    List<OrderItemModel> orders = [
+      OrderItemModel(
+        currency: 'USD',
+        name: 'Apple',
+        price: '4',
+        quantity: 10,
+      ),
+      OrderItemModel(
+        currency: 'USD',
+        name: 'Apple',
+        price: '5',
+        quantity: 12,
+      ),
+    ];
+    /*
+              - Third I created an ItemList of Type ItemListModel and this model holds all orders
+             */
+    var itemList = ItemListModel(orders: orders);
+
+    return (
+      amount: amount,
+      itemList: itemList
+    ); // This is a new data type called records
   }
 }
